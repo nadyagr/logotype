9cf03d7e7560c0e7c573f8dcca72c490f06c8711
nadyagr <62834008+nadyagr@users.noreply.github.com>
2020-04-27 17:57:38 +0300

    logotype

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.slider__inner, .news__slider-inner').slick({
+		prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+		nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+		infinite: false,
+	});
+
+	$('select').styler();
+	$('.header__btn-menu').on('click', function(){
+		$('.menu ul').slideToggle();
+	});
+});
+
+
